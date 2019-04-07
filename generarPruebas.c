e474a0d83369ce32bc745876597a9891d1bb4e5c
Pablo Bernal <49212610+randomtextdude@users.noreply.github.com>
2019-04-07 14:46:32 -0300

    Add files via upload

--- generarPruebas.c
+++ generarPruebas.c
@@ -5,46 +5,89 @@
 #include <assert.h>
 #include "operacionesListas.h"
 
-char **leer_archivo(char *nombreArchivo, int *tamagno) {
-  FILE *archivo = fopen(nombreArchivo, "r");
-  assert(archivo != NULL);
-  int cantLineas = 0, len;
-  char charBuff;
 
-  while ((charBuff = fgetc(archivo)) != EOF) {
-    if (charBuff == '\n') {
-      cantLineas++;
-    }  
-  }
-  
-  rewind(archivo);
+typedef struct _Elemento(){
+  char *id;
+  int ubicacion;
+}Elemento;
 
-  char **arrayStr = malloc(sizeof(char*) * cantLineas);
-  char *strBuff = malloc(sizeof(char) * 50);
+typedef struct _LElementos(){
+  struct _Elemento *elemento;
+  struct _LElemento *sig;
+  int cantidadTotal;
+}ListElem;
 
-  // Copiar strings a arrayw
-
-  fclose(archivo);
-  *tamagno = cantLineas;
-
-  return arrayStr;
+ListElem Crear(){
+  return NULL;
 }
+int ListElem_vacia(ListElem lista) {
+  return lista == NULL;
+}
+
+ListElem agregar_final(ListElem lista, char *dato){
+  int len = strlen(dato);
+
+  ListElem *nuevoNodo = malloc(sizeof(Elemento));
+
+  nuevoNodo->id = malloc(sizeof(char)*(len + 1));
+
+  strcpy(nuevoNodo->id,dato);
+
+  nuevoNodo->sig = NULL;
+
+  if (ListElem_vacia(lista))
+    return nuevoNodo;
+
+  ListElem nodo;
+  for(;!ListElem_vacia(nodo->sig); nodo = nodo->sig);
+
+  nodo->sig = nuevoNodo;
+
+  return lista;
+}
+
+void lectura_archivo(ListElem lista, char *nombre_archivo){
+  FILE *archivo = fopen(nombre_archivo, "r");
+  assert(archivo != NULL);
+  char *strBuff = malloc(sizeof(char) * 50);
+  int posicion = 0;
+  while(fscanf(nombre_archivo) != EOF){
+    fgets(strBuff, 50, archivo);
+    agregar_final(lista, strBuff);
+    ListaElem->elemento->ubicacion = posicion;
+    posicion++;    
+    ListElem->cantidadTotal = posicion;
+  }
+  fclose(archivo);
+  free(strBuff);
+}
+
+
+
+
+
+
 
 
 
 int main(){
   srand(time(NULL));
-  size_t volumenDatos = rand();
-  int tamao=0;
-  char **arrayNombres = leer_archivo("paises.txt", &tamao);
+  size_t volumenDatos = rand() % 5001 + 2000;
+  ListElem listaNombres = Crear();
+  ListElem listaPaises = Crear();
+  FILE *prueba = fopen("censo.txt", "a");
+  lectura_archivo(listaNombres, "nombres.txt");
+  lectura_archivo(listaPaises, "paises.txt");
 
-  printf("%d\n", tamao);
-
-  /*
-  for (int i = 0; i < tamao; i++){
-    printf("%s\n", arrayNombres[i]);
+  for (int i = 0; i < volumenDatos; i++){
+    int ubicNombre = rand() % listaNombres->cantidadTotal;
+    for(;ubicNombre != listaNombres->elemento->ubicacion; listaNombres->sig);
+    int ubicPais = rand() % listaPaises->cantidadTotal;
+    for(;ubicPais != listaPaises->elemento->ubicacion; listaPaises->sig);
+    int edad = rand() % 100 + 1;
+    //ver como agregar eficientemente los datos al archivo
   }
-  */
-
+  fclose(prueba);
+  
   return 0;  
 }
